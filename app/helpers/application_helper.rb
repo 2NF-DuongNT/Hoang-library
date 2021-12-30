9d5955f6bc618045f10b14f9577ed7e28be156a5
hoang0308 <hoangph030898@gmail.com>
2021-12-30 17:24:20 +0700

    add static_pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def full_title(pages_title = "")
+        base_title = "Management Library"
+        if pages_title.empty?
+            base_title
+        else
+            pages_title + " | " + base_title
+        end
+    end
 end
