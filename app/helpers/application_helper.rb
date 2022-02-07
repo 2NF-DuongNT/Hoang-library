91ab9e58ef02da96b0dc33dae6c150187142cfb7
hoang0308 <hoangph030898@gmail.com>
2022-02-07 13:25:40 +0700

    update static_pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,6 @@
 module ApplicationHelper
     def full_title(pages_title = "")
         base_title = "Management Library"
-        if pages_title.empty?
-            base_title
-        else
-            pages_title + " | " + base_title
-        end
+        pages_title.empty? ? (base_title) : (pages_title + " | " + base_title)
     end
 end
